2374d7749d197367e6a0f3b9b90df08c730b1b8d
Ricardo Adorno <ricardo.castrorc1998@gmail.com>
2023-12-09 14:35:23 -0300

    refactor: fix route name

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,6 @@ app.use(express.json());
 
 mongo();
 
-app.use('/', routes.games);
+app.use('/games', routes.games);
 
-app.listen(3000, () => console.log('Server is running!'));
+app.listen(3000, () => console.log('Server is running! http://localhost:3000'));

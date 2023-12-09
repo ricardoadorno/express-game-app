06311e43da8deee3fefd20ae2ef804f107086b48
Ricardo Adorno <ricardo.castrorc1998@gmail.com>
2023-12-09 14:36:03 -0300

    refactor: implements primitive controller to test API

--- src/routes/games.ts
+++ src/routes/games.ts
@@ -1,93 +1,45 @@
 import { Router } from 'express';
+import { GamesModel } from '../models';
 
 const route = Router();
 
-const games = [
-  {
-    id: 1,
-    name: 'Super Mario World',
-    description: 'Best game ever',
-    price: 10,
-  },
-  {
-    id: 2,
-    name: 'Donkey Kong Country',
-    description: 'Second best game ever',
-    price: 15,
-  },
-  {
-    id: 3,
-    name: 'The Legend of Zelda: A Link to the Past',
-    description: 'Third best game ever',
-    price: 20,
-  },
-];
-
-route.get('/', (_, response) => {
-  return response.json(games);
+route.get('/', async (_, response) => {
+  await GamesModel.find({}).then((games) => {
+    return response.json(games);
+  });
 });
 
-route.get('/:id', (request, response) => {
+route.get('/:id', async (request, response) => {
   const { id } = request.params;
 
-  const game = games.find((game) => game.id === Number(id));
-
-  if (!game) {
-    return response.status(404).json({ error: 'Game not found' });
-  }
-
-  return response.json(game);
+  await GamesModel.findById(id).then((game) => {
+    return response.json(game);
+  });
 });
 
-route.post('/', (request, response) => {
-  const { name, description, price } = request.body;
+route.post('/', async (request, response) => {
+  const { body } = request;
 
-  const game = {
-    id: games.length + 1,
-    name,
-    description,
-    price,
-  };
-
-  games.push(game);
-
-  return response.json(game);
+  await GamesModel.create(body).then((game) => {
+    return response.json(game);
+  });
 });
 
-route.put('/:id', (request, response) => {
+route.put('/:id', async (request, response) => {
   const { id } = request.params;
-  const { name, description, price } = request.body;
+  const { body } = request;
 
-  const gameIndex = games.findIndex((game) => game.id === Number(id));
-
-  if (gameIndex < 0) {
-    return response.status(404).json({ error: 'Game not found' });
-  }
-
-  const game = {
-    id: Number(id),
-    name,
-    description,
-    price,
-  };
-
-  games[gameIndex] = game;
-
-  return response.json(game);
+  await GamesModel.updateOne({ _id: id }, body).then((game) => {
+    return response.json(game);
+  });
 });
 
-route.delete('/:id', (request, response) => {
+route.delete('/:id', async (request, response) => {
   const { id } = request.params;
 
-  const gameIndex = games.findIndex((game) => game.id === Number(id));
-
-  if (gameIndex < 0) {
-    return response.status(404).json({ error: 'Game not found' });
-  }
-
-  games.splice(gameIndex, 1);
-
-  return response.status(204).send();
+  await GamesModel.deleteOne({ _id: id }).then((game) => {
+    return response.json(game);
+  });
 });
 
 export default route;

acece7e4139f3670efe78450968a19d1c04a2bc1
Ricardo Adorno <ricardo.castrorc1998@gmail.com>
2023-12-10 00:02:14 -0300

    refactor: implements controller, routes and model para studios. Also a embedded relationship with games

---
+++ src/routes/studios.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import StudiosController from '../controllers/StudiosController';
+
+const route = Router();
+
+route.get('/studios', StudiosController.index);
+
+route.get('/studios/:id', StudiosController.show);
+
+route.post('/studios', StudiosController.store);
+
+route.put('/studios/:id', StudiosController.update);
+
+route.delete('/studios/:id', StudiosController.destroy);
+
+export default route;

d1e8b224330ec31af8df6955fb82584d4a950659
Ricardo Adorno <ricardo.castrorc1998@gmail.com>
2023-12-10 00:02:25 -0300

    refactor: implements controller, routes and model para studios. Also a ref relationship with games

---
+++ src/models/Users.ts
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const usersSchema = new mongoose.Schema(
+  {
+    id: { type: mongoose.Schema.Types.ObjectId },
+    name: String,
+    email: String,
+    password: String,
+    favoriteGame: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'games',
+    },
+  },
+  { versionKey: false }
+);
+
+const Users = mongoose.model('users', usersSchema);
+
+export default Users;
